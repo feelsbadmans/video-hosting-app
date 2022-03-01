8de7239de6ba1b536149543888d2029abf7fdca5
Alexey <grysinn@yandex.ru>
2022-03-01 12:05:57 +0300

    types

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import css from './App.module.scss';
+import css from './App.module.scss'
 
 export const App = () => {
-  return <div className={css.App}>Video Hosting App</div>;
+  return <div className={css.app}>Video Hosting App</div>;
 };
